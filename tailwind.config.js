99ed4f406ddc5d1fc8f023faf1e2c4a7087d63c6
kevinzepwa <zepwakevin8@gmail.com>
2022-12-31 15:14:27 +0300

    ft(navbar & hero)added navbar and hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        'main-blue-color': 'rgba(28,77,122,0.9)',
+      },
+    }
+   },
+   
   plugins: [],
 }
