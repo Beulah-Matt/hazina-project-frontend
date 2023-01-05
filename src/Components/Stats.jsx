c2b32572e3d4cd5f430500950bc674f1618805e2
Beulah Mathenge <beulahmathenge@gmail.com>
2023-01-05 23:25:02 +0300

    Changed styling

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -9,12 +9,12 @@ const Stats = () => {
   }, []);
 
   return (
-    <section className="flex justify-center items-center flex-row flex-wrap bg-gray-700">
+    <section className="flex justify-center items-center flex-row flex-wrap bg-gray-900">
         {stats.map((stat) => (
             // console.log(stats)
             <div key={stat.id} className={`flex-1 flex justify-start items-center flex-row m-3`}>
-                <h4 className="font-poppins font-semibold text-[30px] leading-[43px] text-white">{stat.value}</h4>
-                <p className="font-poppins font-normal text-[15px] leading-[21px] text-cyan-700 uppercase ml-3">{stat.title}</p>
+                <h4 className="font-poppins font-semibold text-[30px] leading-[43px] text-white ml-4" >{stat.value}</h4>
+                <p className="font-poppins font-normal text-[15px] leading-[21px] text-sky-800 uppercase ml-3">{stat.title}</p>
             </div>
         ))}
     </section>
