006dbab6aad7b27ea12a03a6ef1a3e698444050b
Beulah Mathenge <beulahmathenge@gmail.com>
2023-01-10 20:45:25 +0300

    starter code for ft client dashboard

---
+++ src/Components/CustomerDB.jsx
@@ -0,0 +1,11 @@
+import {HiTruck, HiArchive,HiSearch, HiLogout, HiOutlineUser} from "react-icons/hi"
+
+const CustomerDB = () => {
+  return (
+    <div className="bg-sky-800 text-yellow-50 flex justify-center relative">
+        <div className="w-100 max-w-5xl mt-[20px] mb-[100px]"></div>
+    </div>
+  )
+}
+
+export default CustomerDB
