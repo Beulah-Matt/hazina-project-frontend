38b1d62016e3e8bda83703b093ff4f9d736c47f7
Beulah Mathenge <beulahmathenge@gmail.com>
2023-01-20 10:48:57 +0300

    Signin working

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -44,7 +44,7 @@ const Signin = ({setLoggedIn}) => {
             setLoggedIn((prevData)=> ({...prevData, user: {...prevData.user, storages: storages}}))
           })
          
-          navigate("/customers")
+          navigate("/storages")
         });
       } else {
         res.json().then((errors) => {
@@ -71,18 +71,18 @@ const Signin = ({setLoggedIn}) => {
         <div className="mt-10">
           <form action="#" onSubmit={handleSubmit}>
             <div className="flex flex-col mb-6">
-              <label for="email" className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">E-Mail Address:</label>
+              <text for="email" className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Name :</text>
               <div className="relative">
                 <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
                   <svg className="h-6 w-6" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
                   </svg>
                 </div>
-                <input id="name" type="text" name="email"  value={formData.name} className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="E-Mail Address"  onChange={handleChange} />
+                <input id="name" type="text" name="name"  value={formData.name} className="text-sm sm:text-base placeholder-gray-500 pl-10 pr-4 rounded-lg border border-gray-400 w-full py-2 focus:outline-none focus:border-blue-400" placeholder="name"  onChange={handleChange} />
               </div>
             </div>
             <div className="flex flex-col mb-6">
-              <label for="password" className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Password:</label>
+              <label for="password" name="password" value={formData.password} onChange={handleChange} className="mb-1 text-xs sm:text-sm tracking-wide text-gray-600">Password:</label>
               <div className="relative">
                 <div className="inline-flex items-center justify-center absolute left-0 top-0 h-full w-10 text-gray-400">
                   <span>
